f071c202c740be892fa88e250261463e42f362fb
Nommy <nawan7565@gmail.com>
2024-07-08 09:59:08 -0700

    other commit

---
+++ speakhello.js
@@ -0,0 +1,10 @@
+// speakhello.js module 4 javascript
+
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
